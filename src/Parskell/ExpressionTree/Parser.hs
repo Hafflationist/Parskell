136fe264a50d49fadaf804e8feed24c1e9255783
Marcel Robohm <internetzverbindung@gmail.com>
2020-08-28 22:50:48 +0200

    feat: ability to parse parentheses

--- src/Parskell/ExpressionTree/Parser.hs
+++ src/Parskell/ExpressionTree/Parser.hs
@@ -12,10 +12,26 @@ readMaybeUnpack = readMaybe . Data.Text.unpack
 
 
 
-splitOnAndParse :: Text -> BinaryOperator -> Text -> Maybe Expression
-splitOnAndParse operatorText operator text = 
-    let left : rightList = splitOn operatorText text
-        right = Data.Text.intercalate (binaryOperator2Text operator) rightList
+hideThingsInParentheses :: Text -> Text
+hideThingsInParentheses =
+    let mapper count '(' = (count + 1, ' ')
+        mapper count ')' = (count - 1, ' ')
+        mapper count character = (count, if count == 0 then character else ' ')
+    in snd . Data.Text.mapAccumL mapper 0
+
+
+
+splitOnAndParse :: BinaryOperator -> Text -> Maybe Expression
+splitOnAndParse operator text = 
+    let operatorText = binaryOperator2Text operator
+        length = Data.Text.length
+               . Data.Text.intercalate operatorText
+               . Prelude.init
+               . splitOn operatorText 
+               . hideThingsInParentheses 
+               $ text
+        left = Data.Text.take length text
+        right = Data.Text.drop (length + Data.Text.length operatorText) text
     in do 
         leftExpression <- parseExpression left
         rightExpression <- parseExpression right
@@ -23,18 +39,29 @@ splitOnAndParse operatorText operator text =
 
 
 
+trimParentheses :: Text -> Text
+trimParentheses text = 
+    let head = Data.Text.head text
+        last = Data.Text.last text
+    in if (head == '(') && (last == ')')
+       then Data.Text.init . Data.Text.tail $ text
+       else text
+
+
+
 shouldSplitOn :: Text -> Text -> Bool
-shouldSplitOn formula operator = operator `isInfixOf` formula
+shouldSplitOn formula operator = operator `isInfixOf` hideThingsInParentheses formula
+
 
 
 parseExpression :: Text -> Maybe Expression
-parseExpression text
-    | text `shouldSplitOn` Data.Text.pack "+"  = splitOnAndParse (Data.Text.pack "+") Addition text
-    | text `shouldSplitOn` Data.Text.pack "-"  = splitOnAndParse (Data.Text.pack "-") Subtraction text
-    | text `shouldSplitOn` Data.Text.pack "*"  = splitOnAndParse (Data.Text.pack "*") Multiplication text
-    | text `shouldSplitOn` Data.Text.pack "/"  = splitOnAndParse (Data.Text.pack "/") Division text
+parseExpression formula
+    | trimmedFormula `shouldSplitOn` Data.Text.pack "+" = splitOnAndParse Addition trimmedFormula
+    | trimmedFormula `shouldSplitOn` Data.Text.pack "-" = splitOnAndParse Subtraction trimmedFormula
+    | trimmedFormula `shouldSplitOn` Data.Text.pack "*" = splitOnAndParse Multiplication trimmedFormula
+    | trimmedFormula `shouldSplitOn` Data.Text.pack "/" = splitOnAndParse Division trimmedFormula
+    where trimmedFormula = trimParentheses formula
            
 parseExpression const = do 
-    value <- readMaybeUnpack const
+    value <- readMaybeUnpack . trimParentheses $ const
     return (Const (Constant { value = value }))
-                
