138db081269c3d15d89b7dab4be4fdb51084fa9f
Marcel Robohm <internetzverbindung@gmail.com>
2020-09-11 23:35:09 +0200

    feat: added Show instances

--- src/Parskell/ExpressionTree.hs
+++ src/Parskell/ExpressionTree.hs
@@ -21,13 +21,17 @@ data Constant
 instance Eq Constant where
     (==) ConstantFloat {valueFloat = a} ConstantFloat {valueFloat = b} = a == b
     (==) ConstantInteger {valueInteger = a} ConstantInteger {valueInteger = b} = a == b 
-
+instance Show Constant where
+    show ConstantFloat {valueFloat = a} = "(" ++ show a ++ ")" 
+    show ConstantInteger {valueInteger = a} = "(" ++ show a ++ ")" 
 
 data Operation1 = Operator1 {unaryOperator :: UnaryOperator, expression :: Expression}
 instance Eq Operation1 where
     (==) Operator1 {unaryOperator = op1, expression = a1} 
          Operator1 {unaryOperator = op2, expression = a2} = 
          op1 == op2 && a1 == a2
+instance Show Operation1 where
+    show Operator1 {unaryOperator = op, expression = a} = "(" ++ show op ++ " " ++ show a ++ ")"
     
     
 data Operation2 = Operator2 {binaryOperator :: BinaryOperator, expression1 :: Expression, expression2 :: Expression}
@@ -35,6 +39,9 @@ instance Eq Operation2 where
     (==) Operator2 {binaryOperator = op1, expression1 = a11, expression2 = a21} 
          Operator2 {binaryOperator = op2, expression1 = a12, expression2 = a22} =
          op1 == op2 && a11 == a12 && a21 == a22
+instance Show Operation2 where
+    show Operator2 {binaryOperator = op, expression1 = a1, expression2 = a2} = 
+        "(" ++ show a1 ++ " " ++ show op ++ " " ++ show a2 ++ ")"
     
     
 data Operator 
@@ -43,6 +50,9 @@ data Operator
 instance Eq Operator where
     (==) (Unary a1) (Unary a2) = a1 == a2
     (==) _ _ = False
+instance Show Operator where
+    show (Unary unop) = show unop
+    show (Binary unop) = show unop
     
     
 data UnaryOperator 
@@ -52,6 +62,9 @@ instance Eq UnaryOperator where
     (==) Negation Negation = True
     (==) BitNegation BitNegation = True
     (==) _ _ = False
+instance Show UnaryOperator where
+    show Negation = "-"
+    show BitNegation = "~"
 
 data BinaryOperator
     = Addition
@@ -64,6 +77,12 @@ instance Eq BinaryOperator where
     (==) Multiplication Multiplication = True
     (==) Division Division = True
     (==) _ _ = False
+instance Show BinaryOperator where
+    show Addition = "+"
+    show Subtraction = "-"
+    show Multiplication = "*"
+    show Division = "/"
+
 
 data Expression 
     = Const Constant
@@ -75,5 +94,6 @@ instance Eq Expression where
     (==) (Op2 a1) (Op2 a2) = a1 == a2
     (==) _ _ = False
 instance Show Expression where
-    show exp = show "" -- Implement me!!
-    
+    show (Const a) = show a
+    show (Op1 a) = show a
+    show (Op2 a) = show a
